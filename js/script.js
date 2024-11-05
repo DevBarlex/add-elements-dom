3fe605f121cb32422f39e0539d203d80f6ab378e
alexis <alexispaniagua1@gmail.com>
2024-11-06 00:54:12 +0100

    Lista Creada

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
  // Aquí tu código
+
+ const agregar = document.getElementById('agregar');
+ const lista = document.getElementById('lista');
+
+ agregar.addEventListener('click', function () {
+
+    const newitem = prompt('Introduce');
+
+    if (newitem) {
+
+        const listitem = document.createElement('li');
+        listitem.textContent = newitem;
+
+        lista.appendChild(listitem);
+    }else {
+        alert('No se ha ingresado ningún elemento.');
+    }
+ });
